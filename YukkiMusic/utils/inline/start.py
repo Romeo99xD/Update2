3d632efdbad24eec1b6db4151d97506b748590a4
Romeo99xD <94524041+Romeo99xD@users.noreply.github.com>
2022-04-15 12:54:42 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -106,7 +106,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text=_["S_B_7"], user_id=owner"
+                    text=_["S_B_7"], user_id=OWNER"
                 ),
                 InlineKeyboardButton(
                     text=_["ST_B_6"], callback_data="LG"
